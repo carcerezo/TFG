70797a2d0ab0225b1886580791227ab0d43ae7e4
carcerezo <carla.cerezo02@gmail.com>
2025-06-16 20:19:21 +0200

    Update corregir_tfidf.py

--- CODIGOS/corregir_tfidf.py
+++ CODIGOS/corregir_tfidf.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
-# Ruta original del CSV
+# Ruta CSV
 ruta_original = "/Users/carcerezo/Desktop/TFG/DATASET/bigrama_tfidf.csv"
 ruta_corregida = "/Users/carcerezo/Desktop/TFG/DATASET/bigrama_tfidf_corregido.csv"
 
 # Leer CSV
 df = pd.read_csv(ruta_original)
 
-# Convertir a numérico y forzar errores como NaN
+# convertir a numérico 
 df["tfidf"] = pd.to_numeric(df["tfidf"], errors="coerce")
 
-# Eliminar filas donde tfidf sea NaN (los que fallaron)
+# eliminar filas 
 df_limpio = df.dropna(subset=["tfidf"])
 
-# Redondear por estética si lo necesitas
+# redondear 
 df_limpio["tfidf"] = df_limpio["tfidf"].round(4)
 
-# Guardar CSV corregido
+# guardar csv 
 df_limpio.to_csv(ruta_corregida, index=False)
 
-print("✅ Archivo corregido guardado como:")
+print(" Archivo corregido guardado como:")
 print(ruta_corregida)
