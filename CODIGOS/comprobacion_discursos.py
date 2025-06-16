9db976d5c59c11e5ac4d2932f6d24321304ccf46
carcerezo <carla.cerezo02@gmail.com>
2025-06-16 20:18:07 +0200

    Update comprobacion_discursos.py

--- CODIGOS/comprobacion_discursos.py
+++ CODIGOS/comprobacion_discursos.py
@@ -4,20 +4,20 @@ import pandas as pd
 ruta = "/Users/carcerezo/Desktop/TFG/discursos_fomc_limpios.csv"
 df = pd.read_csv(ruta)
 
-# Añadir columna de longitud
+# columna de longitud
 df["longitud"] = df["texto_limpio"].apply(len)
 
-# Ver top 10 más largos
+# 10 mas largos
 print("\n Top 10 discursos más largos:")
 print(df[["año", "archivo", "longitud"]].sort_values(by="longitud", ascending=False).head(10))
 
-# Ver métricas generales
+# metricas generales
 print("\n Métricas generales:")
 print("Longitud mínima:", df["longitud"].min())
 print("Longitud máxima:", df["longitud"].max())
 print("Longitud media:", round(df["longitud"].mean(), 2))
 
-# Mostrar parte del texto más largo
+# texto más largo
 print("\n Fragmento del discurso más largo:")
 ejemplo = df.loc[df["longitud"].idxmax(), "texto_limpio"]
 print(ejemplo[:2000])  # primeros 2000 caracteres
